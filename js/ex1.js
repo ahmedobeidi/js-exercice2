841e8f428dafbe2e3490754d4218f492d945cda2
ahmedobeidi <alobeidi.dev@gmail.com>
2024-11-13 11:31:46 +0100

    ADD: Exercice 3

--- js/ex1.js
+++
@@ -1,2 +0,0 @@
-const name = "Ahmed";
-window.alert(name);
---
+++ js/exercices_1_2.js
@@ -0,0 +1,17 @@
+/* Exercice 1
+const name = "Ahmed";
+window.alert(name);
+*/
+
+/* Exercice 2 
+const firstName = "Ahmed";
+const lastName = "AL-OBEIDI";
+const city = "Saint-Etienne";
+
+window.alert(`${firstName}\n${lastName}\n${city}`);
+*/
+
+/* Exercice 3
+const username = window.prompt("Enter your Name: ");
+window.alert(`Hello ${username}`);
+*/
