e926685b9f8eb10406572d1ec059b0f75b23dff1
ahmedobeidi <alobeidi.dev@gmail.com>
2024-11-13 16:19:35 +0100

    ADD: Exercice 12

---
+++ js/exercices_9_10_11_12.js
@@ -0,0 +1,49 @@
+/* Exercice 9 
+const number = window.prompt("Enter a number: ");
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} * ${i} = ${number * i}`);
+}
+*/
+
+/* Exercice 10 
+const str = window.prompt("Enter a word:");
+
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+}
+*/
+
+/* Exercice 11 
+while (true) {
+    const number = window.prompt("Enter a number between 0 and 10");
+    if (number > 10) {
+        window.alert("Enter a number between 0 and 10");
+        continue;
+    }
+    else if (number < 5) { 
+        window.alert("The number is less than 5");
+        break; 
+    }
+    else if (number > 5) {
+        window.alert("The number is greater than 5");
+        break;  
+    }
+    else { 
+        window.alert("The number is equal 5");
+        break;   
+    }
+}
+*/
+
+/* Exercice 12 
+function square(number) {
+    let result = number ** 2;
+    return result;
+}
+
+const number = window.prompt("Enter a number");
+let result = square(number);
+
+window.alert(`The square of ${number} is ${result}`);
+*/
