3504ce0ed1d35e124b24cd5d36aca635e952ce53
ahmedobeidi <alobeidi.dev@gmail.com>
2024-11-15 09:30:44 +0100

    MODIFY: Exercise 6

--- js/exercice_6.js
+++ js/exercice_6.js
@@ -1,8 +1,12 @@
-function validate() {
+import { calcule } from "./restModule.js";
+
+const submitButton = document.getElementById('submit');
+
+submitButton.addEventListener("click", function() {
     const firstNumber = document.getElementById("firstNumber").value;
     const secondNumber = document.getElementById("secondNumber").value;
 
-    const result = firstNumber % secondNumber;
+    const result = calcule(firstNumber, secondNumber);
     
     window.alert(result);
-}
+});
---
+++ js/restModule.js
@@ -0,0 +1,5 @@
+function calcule(firstNumber, secondNumber) {
+    return firstNumber % secondNumber;
+}
+
+export { calcule }
